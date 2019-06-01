02b27882561bb49dfd61e842e2d4106b8e75c7cf
Dshosev <36865345+Dshosev@users.noreply.github.com>
2019-06-02 00:54:26 +0200

    created the dicee script
    Added the VS Code files to gitignore

---
+++ dicee.py
@@ -0,0 +1,21 @@
+# This litte script is spitting out some random numbers from 1 to 6
+# After printing the number the user will be asked for retry or leave
+
+import random
+
+userWantsMore = 'Y'
+
+while userWantsMore in ['Y', 'y', 'Yes', 'yes', 'YES']:
+  print
+  print('###################################')
+  print
+  print('The first Number is ' + str(random.randint(1,6)) + ' and the second Number is ' + str(random.randint(1,6)))
+  print('Do you want to retry this?')
+  userWantsMore = raw_input('Your answer: ')
+
+print
+print('###################################')
+print
+print('Have a nice day!')
+print
+print('###################################')
